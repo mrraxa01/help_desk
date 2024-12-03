b4e5210af704628a6636f4f1e1d55de0378814ce
Marcio Rodrigues <mrraxa01@gmail.com>
2024-12-02 23:55:04 -0300

    feat:add user entity

---
+++ user-service-api/src/main/java/com/marciorodrigues/user_service_api/entity/User.java
@@ -0,0 +1,27 @@
+package com.marciorodrigues.user_service_api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@Data
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    private Set<ProfileEnum> profile;
+}
