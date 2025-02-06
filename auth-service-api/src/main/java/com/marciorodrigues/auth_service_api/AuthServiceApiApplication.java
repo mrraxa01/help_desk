b9338796645e963b15b668c01d6f3a6a5cd13cb3
Marcio Rodrigues <mrraxa01@gmail.com>
2025-02-05 23:25:28 -0300

    chore: starter auth-service-api

---
+++ auth-service-api/src/main/java/com/marciorodrigues/auth_service_api/AuthServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.marciorodrigues.auth_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApiApplication.class, args);
+	}
+
+}
