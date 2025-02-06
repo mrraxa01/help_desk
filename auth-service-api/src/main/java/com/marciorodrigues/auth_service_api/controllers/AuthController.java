0fb948426ab8474ce8277c62c21887ec539f131f
Marcio Rodrigues <mrraxa01@gmail.com>
2025-02-06 00:45:30 -0300

    feat: implements Authenticate Controller Contract

---
+++ auth-service-api/src/main/java/com/marciorodrigues/auth_service_api/controllers/AuthController.java
@@ -0,0 +1,42 @@
+package com.marciorodrigues.auth_service_api.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import models.exceptions.StandardError;
+import models.requests.AuthenticateRequest;
+import models.responses.AuthenticationResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/auth")
+public interface AuthController {
+
+    @Operation(summary = "Authenticate user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "User authenticated",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthenticationResponse.class))),
+            @ApiResponse(
+                    responseCode = "400", description = "Bad request",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(
+                    responseCode = "401", description = "Unauthorized - Bad Credentials",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(
+                    responseCode = "404", description = "User Not Fault",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+
+            @ApiResponse(
+                    responseCode = "500", description = "Internal Server Error",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+    })
+    @PostMapping("/login")
+    ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody final AuthenticateRequest request);
+
+}
