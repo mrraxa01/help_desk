33421e8ed3897bc676d38ab49ab8012f0797cc40
Marcio Rodrigues <mrraxa01@gmail.com>
2025-03-05 02:00:39 -0300

    chore: add order-server-api

---
+++ order-server-api/src/main/java/com/marciorodrigues/order_server_api/OrderServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.marciorodrigues.order_server_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServerApiApplication.class, args);
+	}
+
+}
---
+++ order-server-api/src/test/java/com/marciorodrigues/order_server_api/OrderServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marciorodrigues.order_server_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
